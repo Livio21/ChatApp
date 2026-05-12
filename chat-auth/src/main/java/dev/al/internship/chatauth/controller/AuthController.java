938d1f9356028da70cf89b226ba16f3eb7ea4c35
livio.macaj <livio.macaj@cit.edu.al>
2026-05-12 17:05:51 +0200

    Excpetion handling http response in  auth endpoints for better frontend legibility.

--- chat-auth/src/main/java/dev/al/internship/chatauth/controller/AuthController.java
+++ chat-auth/src/main/java/dev/al/internship/chatauth/controller/AuthController.java
@@ -5,6 +5,11 @@ import dev.al.internship.chatauth.model.dto.RegisterRequestDto;
 import dev.al.internship.chatauth.model.entity.JwtResponse;
 import dev.al.internship.chatauth.service.AuthService;
 import dev.al.internship.chatauth.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,7 +26,6 @@ public class AuthController {
     public JwtResponse signUp(@RequestBody RegisterRequestDto registerReq) {
         return authService.registerUser(registerReq);
     }
-
     @PostMapping("/auth/sign-in")
     public JwtResponse signIn(@RequestBody LoginRequestDto loginReq) {
         return authService.loginUser(loginReq);
@@ -32,5 +36,15 @@ public class AuthController {
         return "Success";
     }
 
+    @ExceptionHandler(UsernameNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String testException(UsernameNotFoundException e) {
+        return "Username not found" + e.getMessage();
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String testException(BadCredentialsException e) {
+        return "Bad credentials " + e.getMessage();
+    }
 
 }
